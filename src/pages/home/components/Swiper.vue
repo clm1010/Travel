fc8786c6dfd8b969b1b095523df162a2e51160b2
CLM <limin_chu@163.com>
2018-05-28 18:26:36 +0800

    Swiper
    Swiper

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,7 +2,7 @@
 <div class="wrapper">
   <swiper :options="swiperOption">
     <swiper-slide v-for="item of swiperList" :key="item.id">
-      <img class="swiper-img" :src="item.imgUrl" alt="">
+      <img class="swiper-img" :src="item.imgUrl" alt="" />
     </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
   </swiper>
