e997a18cf50709ddeb45fdf3826d4385f60e2ece
CLM <limin_chu@163.com>
2018-06-06 20:13:00 +0800

    Travel
    Travel

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,50 @@
 <template>
-<div class="search">
-  <input class="search-input" type="text" name="" placeholder="输入城市名或拼音" />
+<div>
+  <div class="search">
+    <input v-model="keyword" class="search-input" type="text" name="" placeholder="输入城市名或拼音" />
+  </div>
+  <div class="search-content">
+    <ul>
+      <li v-for="item of list">
+        {{item.name}}
+      </li>
+    </ul>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  // 监听
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -25,4 +63,13 @@ export default {
       text-align: center;
       border-radius: .06rem;
       color: #666;
+  .search-content
+    overflow: hidden;
+    position: absolute;
+    top: 1.58rem;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 1;
+    background-color: green;
 </style>
