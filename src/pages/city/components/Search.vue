91f0ccc05b2aae89a2442e7f1c58551513da61a2
CLM <limin_chu@163.com>
2018-06-06 23:07:27 +0800

    Travel
    Travel

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,17 +3,27 @@
   <div class="search">
     <input v-model="keyword" class="search-input" type="text" name="" placeholder="输入城市名或拼音" />
   </div>
-  <div class="search-content">
+  <div
+    class="search-content"
+    ref="search"
+    v-show="keyword"
+  >
     <ul>
-      <li v-for="item of list">
+      <li
+        class="search-item border-bottom"
+        v-for="item of list"
+        :key=item.id
+      >
         {{item.name}}
       </li>
+      <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
     </ul>
   </div>
 </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
   props: {
@@ -26,11 +36,21 @@ export default {
       timer: null
     }
   },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
   // 监听
   watch: {
     keyword () {
       if (this.timer) {
         clearTimeout(this.timer)
+      }
+      // 清空
+      if (!this.keyword) {
+        this.list = []
+        return
       }
       this.timer = setTimeout(() => {
         const result = []
@@ -44,6 +64,10 @@ export default {
         this.list = result
       }, 100)
     }
+  },
+  // 生命周期DOM挂载之后执行
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
   }
 }
 </script>
@@ -71,5 +95,10 @@ export default {
     left: 0;
     bottom: 0;
     z-index: 1;
-    background-color: green;
+    background-color: #EEE;
+    .search-item
+      line-height: .62rem;
+      padding-left: .2rem;
+      color: #666;
+      background-color: #FFF;
 </style>
